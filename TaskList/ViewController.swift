1cf3733b3dd0f6fc31468a5b130fec7d4cf50fd8
nika <78690921+Niklaus2000@users.noreply.github.com>
2024-01-21 21:07:33 +0400

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by MacBoobPro on 21.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
